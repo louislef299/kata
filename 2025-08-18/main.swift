61de981868c8283a800fad49afa559f5c960a1e1
Louis Lefebvre <lefebl4@medtronic.com>
2025-08-18 12:17:00 -0500

    chore: Add kata for my first swift programs

---
+++ 2025-08-18/main.swift
@@ -0,0 +1,10 @@
+print("Hello, world!")
+
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let explicitDouble: Double = 70
+print(explicitDouble)
+print("The value of myVariable is \(myVariable) and the value of explicitDouble is \(explicitDouble)")
+

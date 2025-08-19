f591e83ded11f3e7d3da09f4c8fd43a22cc17453
Louis Lefebvre <lefebl4@medtronic.com>
2025-08-19 15:32:10 -0500

    feat: Add swift hello world vapor rest svc

--- 2025-08-18/main.swift
+++ 2025-08-18/main.swift
@@ -8,3 +8,18 @@ let explicitDouble: Double = 70
 print(explicitDouble)
 print("The value of myVariable is \(myVariable) and the value of explicitDouble is \(explicitDouble)")
 
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+// Prints "false"
+
+var optionalName: String? = nil
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+print(greeting)
+
+let nickname: String? = nil
+let fullName: String = "John Appleseed"
+print("Hi \(nickname ?? fullName)")
+
